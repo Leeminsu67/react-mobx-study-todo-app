86b7c7ffbb9b4468b4811a35b896939dda28bda9
Leeminsu67 <alstn67@gmail.com>
2024-05-12 23:59:09 +0900

    mobx tsx study todo app

---
+++ src/TodoList.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import TodoStore from "./TodoStore";
+import { observer } from "mobx-react-lite";
+
+interface TodoListProps {
+  todoStore: TodoStore;
+}
+
+const TodoList: React.FC<TodoListProps> = observer(({ todoStore }) => {
+  const [value, setValue] = useState<string>("");
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <input
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+        type="text"
+      />
+      <button
+        onClick={() => {
+          if (value) {
+            todoStore.addTodo(value);
+          }
+          setValue("");
+        }}
+      >
+        Add
+      </button>
+
+      <div>Completed: {todoStore.status.completed}</div>
+      <div>Remaining: {todoStore.status.remaining}</div>
+
+      <ul>
+        {todoStore.todos.map((todo) => {
+          return (
+            <li
+              key={todo.id}
+              onClick={() => {
+                todoStore.toggleTodo(todo.id);
+              }}
+            >
+              {todo.title}[{todo.completed ? "x" : " "}]
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+});
+
+export default TodoList;
